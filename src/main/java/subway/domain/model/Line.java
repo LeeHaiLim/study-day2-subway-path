99af41e50244cf487583529f9146139419a308aa
DESKTOP-BPLVUF3\user <jackk0616@gmail.com>
2022-12-04 15:41:28 +0900

    feat(Line): 노선에 노선이 가지고 있을 역들을 추가시키고 생성자도 맞춰서 추가

--- src/main/java/subway/domain/model/Line.java
+++ src/main/java/subway/domain/model/Line.java
@@ -1,10 +1,21 @@
 package subway.domain.model;
 
+
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations;
 
     public Line(String name) {
         this.name = name;
+    }
+
+    public Line(String name, Station... stations) {
+        this.name = name;
+        for(Station station : stations) {
+            this.stations.add(station);
+        }
     }
 
     public String getName() {
