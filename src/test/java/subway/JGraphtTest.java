5757eb6ebd9c3e83a4b3b0f5b55351a809ef9b41
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-12-04 13:01:15 +0900

    test(JGrapht) : 객체에서도 잘 작동하늕 확인하기 위한 테스트 실시

--- src/test/java/subway/JGraphtTest.java
+++ src/test/java/subway/JGraphtTest.java
@@ -4,6 +4,7 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
+import subway.domain.Station;
 
 import java.util.List;
 
@@ -22,6 +23,31 @@ public class JGraphtTest {
 
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+
+        for (String station : shortestPath) {
+            System.out.println(station);
+        }
+        assertThat(shortestPath.size()).isEqualTo(3);
+    }
+
+    @Test
+    void getDijkstraShortestPath_ObjectTest() {
+        Station station1 = new Station("11역");
+        Station station2 = new Station("22역");
+        Station station3 = new Station("33역");
+
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex(station1);
+        graph.addVertex(station2);
+        graph.addVertex(station3);
+        graph.addVertex(station1);
+
+        graph.setEdgeWeight(graph.addEdge(station1, station2), 2);
+        graph.setEdgeWeight(graph.addEdge(station2, station3), 2);
+        graph.setEdgeWeight(graph.addEdge(station1, station3), 100);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<Station> shortestPath = dijkstraShortestPath.getPath(station1, station3).getVertexList();
 
         assertThat(shortestPath.size()).isEqualTo(3);
     }
