f995b32c4a6fe5e7a252c99101f971d945ce5295
DESKTOP-BPLVUF3\user <jackk0616@gmail.com>
2022-12-04 16:40:47 +0900

    feat(OptimalPathFinder): 출발역과 도착역을 받아서 최적의 경로를 찾아 Path 객체로 넘겨주는 클래스

---
+++ src/main/java/subway/OptimalPathFinder.java
@@ -0,0 +1,70 @@
+package subway;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import subway.domain.model.Path;
+import subway.domain.model.Section;
+import subway.domain.model.Station;
+import subway.domain.repository.SectionRepository;
+import subway.domain.repository.StationRepository;
+
+import java.util.List;
+
+public class OptimalPathFinder {
+
+    public static final WeightedMultigraph<Station, DefaultWeightedEdge> shortestDistanceGraph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    public static final WeightedMultigraph<Station, DefaultWeightedEdge> minimunTimeGraph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+    public void init() {
+        for (int i = 0; i < StationRepository.stations().size(); i++) {
+            shortestDistanceGraph.addVertex(StationRepository.stations().get(i));
+            minimunTimeGraph.addVertex(StationRepository.stations().get(i));
+        }
+
+        for (int i = 0; i < SectionRepository.sections().size(); i++) {
+            Section section = SectionRepository.sections().get(i);
+            shortestDistanceGraph.setEdgeWeight(shortestDistanceGraph.addEdge(section.getUpBoundStation(),
+                    section.getDownBoundStation()), section.getDistance());
+            minimunTimeGraph.setEdgeWeight(minimunTimeGraph.addEdge(section.getUpBoundStation(),
+                    section.getDownBoundStation()), section.getTime());
+        }
+    }
+
+    public Path getRoute(Station startStation, Station endStation, String command) {
+        List<Station> stations = findStations(startStation, endStation, command);
+        int distance = getDistance(stations);
+        int time = getTime(stations);
+        return new Path(time, distance, stations);
+    }
+
+    private List<Station> findStations(Station startStation, Station endStation, String command) {
+        DijkstraShortestPath dijkstraShortestPath;
+        if (command.equals("1")) {
+            dijkstraShortestPath = new DijkstraShortestPath(shortestDistanceGraph);
+            return dijkstraShortestPath.getPath(startStation, endStation).getVertexList();
+        }
+        if (command.equals("2")) {
+            dijkstraShortestPath = new DijkstraShortestPath(minimunTimeGraph);
+            return dijkstraShortestPath.getPath(startStation, endStation).getVertexList();
+        }
+        throw new IllegalArgumentException("[ERROR] 잘못된 커맨드 입력");
+    }
+
+    public int getDistance(List<Station> stations) {
+        int distance = 0;
+        for(int i = 0; i < stations.size() - 1; i++) {
+            distance += shortestDistanceGraph.getEdgeWeight(shortestDistanceGraph.getEdge(stations.get(i), stations.get(i + 1)));
+        }
+        return distance;
+    }
+
+    public int getTime(List<Station> stations) {
+        int time = 0;
+        for(int i = 0; i < stations.size() - 1; i++) {
+            time += minimunTimeGraph.getEdgeWeight(minimunTimeGraph.getEdge(stations.get(i), stations.get(i + 1)));
+        }
+        return time;
+    }
+
+}
