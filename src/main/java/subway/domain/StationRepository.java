a31a459c723008dbb7ec8a3a362ce3c55e5a1d17
DESKTOP-BPLVUF3\user <jackk0616@gmail.com>
2022-12-04 12:42:29 +0900

    feat(StationRepository): 이름으로 역을 찾는 기능 구현

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -1,19 +1,23 @@
 package subway.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
+
+    public static void addStation(Station station) {
+        stations.add(station);
+    }
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
 
-    public static void addStation(Station station) {
-        stations.add(station);
+    public static Station findByName(String name) {
+        return stations.stream()
+                .filter(station -> name.equals(station.getName()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 입력된 이름의 역이 존재하지 않습니다."));
     }
 
     public static boolean deleteStation(String name) {
