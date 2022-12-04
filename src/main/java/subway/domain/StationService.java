7d3dc17bc68d4354522055494db4c264dfe3a33e
DESKTOP-BPLVUF3\user <jackk0616@gmail.com>
2022-12-04 12:58:43 +0900

    feat(StationService): crud에 해당하는 메소드 구현

--- src/main/java/subway/domain/StationService.java
+++ src/main/java/subway/domain/StationService.java
@@ -1,7 +1,8 @@
 package subway.domain;
 
-public class StationService {
+import java.util.List;
 
+public class StationService {
 
     public void registerStation(String name) {
         if(StationRepository.findByName(name) != null) {
@@ -10,5 +11,23 @@ public class StationService {
         StationRepository.addStation(new Station(name));
     }
 
+    public void deleteStation(String name) {
+        if(StationRepository.findByName(name) == null) {
+            throw new IllegalArgumentException("[ERROR] 입력하신 역은 존재하지 않습니다.");
+        }
+        StationRepository.deleteStation(name);
+    }
+
+    public List<Station> findAll() {
+        return StationRepository.stations();
+    }
+
+    public Station findByName(String name) {
+        return StationRepository.findByName(name);
+    }
+
+    public void deleteAll() {
+        StationRepository.deleteAll();
+    }
 
 }
